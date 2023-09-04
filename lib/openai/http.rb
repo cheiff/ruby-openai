f5a2ea32c5da9fce94997e3a14b925bd3577822e
Javito <javier.olivares@getro.com>
2023-09-04 11:46:37 +0200

    Apply https://github.com/ScotterC/ruby-openai/blob/dewey/lib/openai/http.rb\#L9-L31 patch

--- lib/openai/http.rb
+++ lib/openai/http.rb
@@ -7,17 +7,27 @@ module OpenAI
     end
 
     def json_post(path:, parameters:)
-      to_json(conn.post(uri(path: path)) do |req|
+      raw = parameters[:raw] if parameters.key?(:raw)
+      response = conn.post(uri(path: path)) do |req|
         if parameters[:stream].respond_to?(:call)
           req.options.on_data = to_json_stream(user_proc: parameters[:stream])
           parameters[:stream] = true # Necessary to tell OpenAI to stream.
         elsif parameters[:stream]
           raise ArgumentError, "The stream parameter must be a Proc or have a #call method"
         end
+        req.headers = headers.merge(parameters[:headers] || {})
 
-        req.headers = headers
-        req.body = parameters.to_json
-      end&.body)
+        request_body = parameters.except(:raw, :headers)
+        req.body = request_body.to_json
+      end
+
+      return nil unless response
+
+      if raw
+        response.to_hash.merge(body: to_json(response.to_hash[:body]))
+      else
+        to_json(response.body)
+      end
     end
 
     def multipart_post(path:, parameters: nil)
